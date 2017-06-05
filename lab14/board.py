df4362c1efb27defbef34193e6427a3eddaedbf4
martalozynska <martuha.lozinska@gmail.com>
2017-06-06 01:28:33 +0300

    Add files via upload

---
+++ lab14/board.py
@@ -0,0 +1,16 @@
+class Board:
+    EMPTY_CELL = 0
+    O_CELL = 1
+    x_CELL = 2
+    x_wins = 3
+    O_wins = 4
+    DRAW = 5
+    NOT_ENDED = 6
+    def __init__(self):
+        self.field = 
+        print(self.str_field)
+    def put(self,turn, coord):
+        self.turn = turn
+        self.coord = coord
+
+l = Board()
